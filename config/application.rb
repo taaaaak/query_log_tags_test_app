cd43db7b63520d5574adf58a3428e510d4352138
user <user@DESKTOP-3VUHBJT.localdomain>
2022-07-21 23:18:32 +0900

    change constant query_log_tags_enabled true

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.active_record.query_log_tags_enabled = true
 
     # Configuration for the application, engines, and railties goes here.
     #
